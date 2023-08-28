be6ff7ce691f1fff0b157cd3422e953f819dcd3f
easynotedev <ktilaon@gmail.com>
2023-08-28 21:19:08 +0800

    added content

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+// components/Hero.jsx
+
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <Image src='/images/profile.jpeg' className="profile-img" width={300} height={300} alt="Antonin's personal headshot" />
+      <div className="hero-text">
+        <h1>Hey, I'm Antonin 👋🏝️🌅</h1>
+        <p>
+          I'm a associate software developer based in Palawan, Philippines. I currently certifying myself to be a front-end web developer.
+          I have associate industry experience, eager to put my skills into practice.
+        </p>
+        <div className="social-icons">
+          <a
+            href="https://github.com/easynotedev"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/aktilaon/"
+            aria-label="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
+import Hero from '@/components/Hero';
+
+import Head from 'next/head';
+
 const Home = () => {
-	return <div>{/* Add components Here */}</div>;
+	return (
+		<>
+			<Head>
+				<title>Antonin's Portfolio</title>
+				<meta name="description" content="Joel's Portfolio" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.jpeg" />
+			</Head>
+			<div>
+				<Hero />
+				{/*} <About />
+				<Skills />
+				<Projects />
+				<Contact />  */}
+			</div>
+		</>
+	);
 };
 
 export default Home;
