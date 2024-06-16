074be67b809220c56586a0066bdc8e949d4ad947
easynotedev <ktilaon@gmail.com>
2024-06-16 23:28:26 +0800

    change hero and about content; deleted classier in server

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,21 +9,15 @@ const About = () => {
       <div className="flex-about">
         <div className="about-text">
           <p> 
-            I have deep experience with data analysis software such as Excel and SQL, 
+            I have experience with data extraction software such as Excel and SQL, 
             I am currently certifying myself in data analysis using Power BI, 
-            my goal is to keep upgrading my qualifications until I become a senior-level data analyst. 
+            my goal is to keep upgrading my qualifications until I become a senior-level software engineer. 
             My past experience in Accenture and NorthgateArinso are also work experience that are tied 
-            to data analysis and KPI report creation. In both instances, I maintained proprietary software, 
-            making sure that no downtime will be reached by creating up to date reports and metrics 
-            to gauge software bottlenecks.
+            to software development . In both instances, I maintained proprietary software, 
+            making sure that no downtime by engineering solutions and analytical pinpointing root causes.
           </p>
-          <p>
-            As a developer, I have a strong foundation in software development, with a focus on web
-            technologies such as HTML, CSS, and JavaScript. I enjoy working on both the front-end and
-            back-end of applications, and I am always looking for ways to optimize performance, improve user
-            experience, and ensure the highest level of code quality.
-          </p>
-          <p>I have worked on a wide range of projects, from simple static websites to
+          <p>As a developer, I have a strong foundation in software development, with a focus on web
+          technologies such as Frameworks and cloud technologies. I have worked on a wide range of projects, from simple static websites to
             complex enterprise-level applications. I am experienced in working with web development
             tools and frameworks, including React and Node.js. I am always eager
             to learn and explore new technologies, and I am constantly seeking out opportunities to improve
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,7 @@ const Hero = () => {
       <div className="hero-text">
         <h1>Hey, I'm Antonin 👋🏝️🌅</h1>
         <p>
-          I'm a software engineer based in Palawan, Philippines. I currently certifying myself to be a web developer and data analyst.
-          I have industry experience in data analysis and software development, eager to put my skills into a work environment. I have understanding of Machine Learning and
+          I'm a software engineer based in Palawan, Philippines. I have industry experience in ETL data and software development, eager to put my skills into a work environment. I have understanding of Machine Learning and
           it's web technologies applications.
         </p>
         <div className="social-icons">
